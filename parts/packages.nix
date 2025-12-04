2ebda8bf12a0c4c901f3cd82a7739a8289f06a1f
Andrew Thomas Lee <andrewthomaslee.business@gmail.com>
2025-12-04 09:43:40 -0600

    update nginx cache controls and make prod ready

--- parts/packages.nix
+++ parts/packages.nix
@@ -22,25 +22,39 @@
         daemon off;
         error_log /dev/stderr;
         pid /dev/null;
-        events {}
+
+        events {
+          worker_connections 1024;
+        }
+
         http {
           access_log /dev/stdout;
           include ${pkgs.nginx}/conf/mime.types;
+
+          gzip on;
+          gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
+          gzip_min_length 1000;
+
           open_file_cache max=1000 inactive=600s;
           open_file_cache_valid 60m;
           open_file_cache_errors on;
+
           server {
             listen ${toString nginxPort};
             root ${hugo-package}/var/www;
-            location ~* \.(css|js|png|jpg|svg|ico)$ {
+
+            error_page 404 /404.html;
+
+            location ~* \.(css|js|png|jpg|svg|ico|woff2)$ {
               expires 1y;
-              add_header Cache-Control "public, max-age=31536000";
+              add_header Cache-Control "public, max-age=31536000, immutable";
               access_log off;
             }
+
             location / {
               try_files $uri $uri/ =404;
-              expires 1y;
-              add_header Cache-Control "public, max-age=31536000";
+              expires 1h;
+              add_header Cache-Control "public, max-age=3600";
             }
           }
         }
